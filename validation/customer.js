b319d29f25404ed43cbbdb24749623e954d33df6
shafayeatt1995 <shafayetalanik@gmail.com>
2025-07-05 17:34:41 +0600

    Customer updated

--- validation/customer.js
+++ validation/customer.js
@@ -5,6 +5,7 @@ const validate = {
     check("id").trim().notEmpty().withMessage("ID is required"),
     check("name").trim().notEmpty().withMessage("Name is required"),
     check("phone").trim().notEmpty().withMessage("Phone is required"),
+    check("zoneID").trim().notEmpty().withMessage("Zone is required"),
   ],
 };
 
