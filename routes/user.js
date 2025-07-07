ef68998fbb76842bdbce8593cb1b62869775afff
shafayeatt1995 <shafayetalanik@gmail.com>
2025-07-08 05:05:02 +0600

    refresh token aded

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { User, Business } = require("../models");
+const { sendError, message } = require("../utils");
+const jwt = require("jsonwebtoken");
 
 router.get("/", async (req, res) => {
   try {
@@ -19,6 +21,55 @@ router.get("/", async (req, res) => {
     return res.send(body);
   } catch (error) {
     return res.status(500).json({ error: error.message });
+  }
+});
+router.get("/refresh-token", async (req, res) => {
+  try {
+    const bearer = req.headers.authorization || "";
+    if (!bearer) return sendError({ message: "token not found" });
+    const oldToken =
+      bearer.split(" ")[0].toLowerCase() === "bearer"
+        ? bearer.split(" ")[1]
+        : null;
+
+    const validateToken = jwt.verify(oldToken, process.env.AUTH_SECRET);
+    if (!validateToken) throw new Error(`token isn't valid`);
+    const user = await User.findOne({ _id: validateToken._id }).select(
+      "+power"
+    );
+    let business = null;
+    if (user.businessID) {
+      business = await Business.findOne({
+        _id: user.businessID,
+        exp: { $gte: new Date() },
+      }).lean();
+    }
+    const { _id, id, name, power, type } = user;
+
+    const payload = {
+      _id,
+      id,
+      name,
+    };
+
+    if (power === 420 && type === "admin") payload.isAdmin = true;
+    if (business) {
+      payload.businessID = business._id;
+      const _idStr = _id.toString();
+      if (business.ownerIDs.some((id) => id.toString() === _idStr))
+        payload.isOwner = true;
+      if (business.staffIDs.some((id) => id.toString() === _idStr))
+        payload.isStaff = true;
+    }
+
+    const token = jwt.sign(payload, process.env.AUTH_SECRET, {
+      expiresIn: "30 days",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message });
   }
 });
 
